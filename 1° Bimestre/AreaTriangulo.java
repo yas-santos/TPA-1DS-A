f48128fdbdfda83b16f865115fbfbb6d550deb36
Yasmin Santos <yasmingeici@gmail.com>
2025-11-25 18:26:01 -0300

    Add files via upload

---
+++ 1° Bimestre/AreaTriangulo.java
@@ -0,0 +1,14 @@
+package prjAula01;
+
+public class AreaTriangulo {
+	public static void main(String[] args) {
+		int valorbase,valorh,area;
+		valorbase=63;
+		valorh=12;
+		area=valorbase*valorh;
+		System.out.println(area);
+		
+		
+	}
+
+}
---
+++ 1° Bimestre/OlaMundo.java
@@ -0,0 +1,8 @@
+package prjAula01;
+
+public class OlaMundo {
+	public static void main (String[] args ) {
+			System.out.println("Olá Mundo!");
+			System.out.println("Hello World!");
+	}
+}
